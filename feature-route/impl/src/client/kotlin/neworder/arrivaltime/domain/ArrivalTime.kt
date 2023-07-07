48e2b5221feb97aa6633d2e4bf8c318a84359636
Dmitr.Okunev <Dmitr.Okunev@x5.ru>
2023-07-07 12:14:22 +0200

    DEL-288 изменил временные интервалы

--- feature-route/impl/src/client/kotlin/neworder/arrivaltime/domain/ArrivalTime.kt
+++ feature-route/impl/src/client/kotlin/neworder/arrivaltime/domain/ArrivalTime.kt
@@ -1,13 +1,13 @@
 package neworder.arrivaltime.domain
 
 enum class ArrivalTime(val range: String) {
-    One("С 8:00 до 10:00"),
+    One("С 08:00 до 10:00"),
     Two("С 10:00 до 12:00"),
     Three("С 12:00 до 14:00"),
     Four("С 14:00 до 16:00"),
     Five("С 16:00 до 18:00"),
     Six("С 18:00 до 20:00"),
     Seven("С 20:00 до 22:00"),
-    Day("С 9:00 до 18:00"),
+    Day("С 09:00 до 18:00"),
     Evening("С 16:00 до 21:00"),
 }
